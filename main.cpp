25f6c568a087457cd6fbeef849bed9c85a0d7403
Mr.Atom <natvaches@gmail.com>
2023-04-18 13:19:06 +0400

    first commit - save homework

---
+++ main.cpp
@@ -0,0 +1,14 @@
+﻿#include "inout.h" //подключаю заголовочный файл и нужные библиотеки
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus"); // меняю локализацию
+	vector <int> vec; // создание вектора
+	read(vec); // вызов функции для записи значений в вектор
+	sort(vec.begin(), vec.end()); // сортировка вектора
+	print(vec); // вывод в консоль элементов вектора
+	return 0;
+}
